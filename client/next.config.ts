25efe475f9119cc79bfa65693cd25e9329ba4cc8
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-10-02 14:35:43 +0530

    deployment 0.07

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',           // enables static export
-  basePath: '/My-Portfolio---HTML-CSS-JavaScript-',
-  assetPrefix: '/My-Portfolio---HTML-CSS-JavaScript-/', // add this
+ output: "export",           
+  basePath: "/My-Portfolio---HTML-CSS-JavaScript-",
+  assetPrefix: "/My-Portfolio---HTML-CSS-JavaScript-/", 
+  images: {
+    unoptimized: true,  // Required for Next.js Image on static exports
+  },
 };
 
 export default nextConfig;
