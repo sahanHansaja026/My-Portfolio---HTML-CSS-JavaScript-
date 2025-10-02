3e79266da92c9cb490b2f4ef191033a9cf992914
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-10-02 19:13:06 +0530

    deployment 0.14

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -11,8 +11,38 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
   return (
     <html lang="en">
+      <head>
+        <style>{`
+          @font-face {
+            font-family: "Inknut Antiqua";
+            src: url("${basePath}/fonts/InknutAntiqua-Regular.ttf") format("truetype");
+            font-weight: 400;
+            font-style: normal;
+          }
+          @font-face {
+            font-family: "Inknut Antiqua";
+            src: url("${basePath}/fonts/InknutAntiqua-Bold.ttf") format("truetype");
+            font-weight: 700;
+            font-style: normal;
+          }
+          @font-face {
+            font-family: "Inder";
+            src: url("${basePath}/fonts/Inder-Regular.ttf") format("truetype");
+            font-weight: 400;
+            font-style: normal;
+          }
+          @font-face {
+            font-family: "Inter";
+            src: url("${basePath}/fonts/Inter-VariableFont_opsz,wght.ttf") format("truetype");
+            font-weight: 400;
+            font-style: normal;
+          }
+        `}</style>
+      </head>
       <body className="antialiased">
         {children}
       </body>
