4aacc14f28e1f0ae76ebcfe1965eb2bc560d73b3
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-10-02 14:12:45 +0530

    deployment 0.04

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',           // enables static export
   basePath: '/My-Portfolio---HTML-CSS-JavaScript-',
+  assetPrefix: '/My-Portfolio---HTML-CSS-JavaScript-/', // add this
 };
 
 export default nextConfig;
